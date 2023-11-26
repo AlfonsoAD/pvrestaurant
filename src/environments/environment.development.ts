92ebf4c01bb30eef243135430da9288016bd504b
AlfonsoAD <alfonsoad097@gmail.com>
2023-11-26 16:52:42 -0600

    Envs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // baseURL: 'https://devapipvrestaurant.onrender.com/api/',
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://devapipvrestaurant.onrender.com/api/',
+  // baseURL: 'http://localhost:8000/api/',
   cloudinaryURL: 'https://res.cloudinary.com/dhbyqfmge/',
 };
